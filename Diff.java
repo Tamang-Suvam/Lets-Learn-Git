498b112d884c1f04d2bd18c5319f56f5a829d311
Tamang-Suvam <tamangsuvam10@gmail.com>
2023-06-15 10:16:35 +0530

    minor change in Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,6 @@ public class Diff {
   public staic void main(String [] args) {
     int x = 5;
     int y = 6;
-    System.out.println("The Difference is: " + x - y);
+    System.out.println("The Diff is: " + x - y);
   }
 }
