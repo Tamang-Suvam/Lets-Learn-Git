1c13c266feea2c3e84656d03fb01c665c17d7d8b
Tamang-Suvam <tamangsuvam10@gmail.com>
2023-06-15 09:31:02 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
-class Sum {
+public class Sum {
   public staic void main(String [] args) {
     int x = 5;
     int y = 6;
-    System.out.println("Sum: " + x + y);
+    System.out.println("The Sum is: " + x + y);
   }
 }
